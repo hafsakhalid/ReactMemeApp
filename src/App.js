e424ff05f96da19a7474bc4fd74a8ab18d648e10
Hafsa Khalid <hafsa.khalid@mail.mcgill.ca>
2020-08-12 14:29:36 -0400

    lets goo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from "./Header";
+
+function App() { 
+    return ( 
+        <div>
+        <Header/>
+        </div>
+    )
+
+}
+export default App;
